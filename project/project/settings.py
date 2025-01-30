20b38d9672c77a1db269b23753a9cf08a2d09964
Muhammad Mohsin <142692213+Volcann@users.noreply.github.com>
2025-01-30 18:43:14 +0500

    Done

--- project/project/settings.py
+++ project/project/settings.py
@@ -120,7 +120,7 @@ AUTH_USER_MODEL = 'api.UserProfile'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
