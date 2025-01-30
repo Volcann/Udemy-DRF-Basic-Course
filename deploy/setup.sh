09864184ff4efbf0838efb87c5745f9bcfbea868
Muhammad Mohsin <142692213+Volcann@users.noreply.github.com>
2025-01-30 18:32:02 +0500

    Updated

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -2,39 +2,41 @@
 
 set -e
 
-# TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/CHANGEME'
-
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+# Set your Git repository URL
+PROJECT_GIT_URL='https://github.com/Volcann/Udemy-course-project.git'
+PROJECT_BASE_PATH='/usr/local/apps/Udemy-course-project'
+DJANGO_PROJECT_PATH="$PROJECT_BASE_PATH/project"
 
 # Set Ubuntu Language
 locale-gen en_GB.UTF-8
 
-# Install Python, SQLite and pip
+# Install dependencies
 echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 
+# Clone project
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH
 
+# Setup Virtual Environment
 python3 -m venv $PROJECT_BASE_PATH/env
+$PROJECT_BASE_PATH/env/bin/pip install -r $DJANGO_PROJECT_PATH/requirements.txt uwsgi==2.0.21
 
-$PROJECT_BASE_PATH/env/bin/pip install -r $PROJECT_BASE_PATH/requirements.txt uwsgi==2.0.21
+# Run Django migrations & collect static files
+$PROJECT_BASE_PATH/env/bin/python $DJANGO_PROJECT_PATH/manage.py migrate
+$PROJECT_BASE_PATH/env/bin/python $DJANGO_PROJECT_PATH/manage.py collectstatic --noinput
 
-# Run migrations
-$PROJECT_BASE_PATH/env/bin/python $PROJECT_BASE_PATH/manage.py migrate
-
-# Setup Supervisor to run our uwsgi process.
-cp $PROJECT_BASE_PATH/deploy/supervisor_profiles_api.conf /etc/supervisor/conf.d/profiles_api.conf
+# Setup Supervisor
+cp $DJANGO_PROJECT_PATH/deploy/supervisor_api.conf /etc/supervisor/conf.d/api.conf
 supervisorctl reread
 supervisorctl update
-supervisorctl restart profiles_api
+supervisorctl restart api
 
-# Setup nginx to make our application accessible.
-cp $PROJECT_BASE_PATH/deploy/nginx_profiles_api.conf /etc/nginx/sites-available/profiles_api.conf
+# Setup Nginx
+cp $DJANGO_PROJECT_PATH/deploy/nginx_api.conf /etc/nginx/sites-available/api.conf
 rm /etc/nginx/sites-enabled/default
-ln -s /etc/nginx/sites-available/profiles_api.conf /etc/nginx/sites-enabled/profiles_api.conf
+ln -s /etc/nginx/sites-available/api.conf /etc/nginx/sites-enabled/api.conf
 systemctl restart nginx.service
 
-echo "DONE! :)"
+echo "Deployment Completed! 🚀"
--- deploy/update.sh
+++ deploy/update.sh
@@ -2,12 +2,39 @@
 
 set -e
 
-PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
+# Define the correct project base path for your local machine
+PROJECT_BASE_PATH='/Users/volcann/Downloads/Udemy-course-project'
+DJANGO_PROJECT_PATH="$PROJECT_BASE_PATH/project"
 
+# Change to the project directory
 cd $PROJECT_BASE_PATH
-git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
-supervisorctl restart profiles_api
 
-echo "DONE! :)"
+# Pull the latest changes from Git
+echo "Pulling the latest changes from Git..."
+git pull origin main
+
+# Activate virtual environment
+echo "Activating virtual environment..."
+source $PROJECT_BASE_PATH/env/bin/activate
+
+# Install dependencies from requirements.txt
+echo "Installing dependencies..."
+$PROJECT_BASE_PATH/env/bin/pip install -r $DJANGO_PROJECT_PATH/requirements.txt
+
+# Run database migrations
+echo "Running migrations..."
+$PROJECT_BASE_PATH/env/bin/python $DJANGO_PROJECT_PATH/manage.py migrate
+
+# Collect static files
+echo "Collecting static files..."
+$PROJECT_BASE_PATH/env/bin/python $DJANGO_PROJECT_PATH/manage.py collectstatic --noinput
+
+# Restart Supervisor (if applicable)
+echo "Restarting Supervisor..."
+supervisorctl restart api
+
+# Restart Nginx (if applicable)
+echo "Restarting Nginx..."
+systemctl restart nginx
+
+echo "Update completed successfully! 🚀"
