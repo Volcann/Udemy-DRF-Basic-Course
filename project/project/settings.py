f725839e18313ad1ade3e385685df8c59af44795
Muhammad Mohsin <142692213+Volcann@users.noreply.github.com>
2025-02-02 01:22:37 +0500

    Updated the database for sqlite to postgreSQL

--- project/project/settings.py
+++ project/project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend_course',  # Correct database name
+        'USER': 'mysuperuser',
+        'PASSWORD': 'mysuperuser',
+        'HOST': 'backend.cjoykeq4wnrp.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
